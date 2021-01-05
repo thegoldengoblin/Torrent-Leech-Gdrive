337f96482259580d2c1af9463e305656797c1275
thegoldengoblin <69628636+thegoldengoblin@users.noreply.github.com>
2021-01-05 13:04:24 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+from tobrot.sample_config import Config
+
+#Fill your all data, read readme for reference
+
+class Config(Config):
+
+	TG_BOT_TOKEN= "1538141688:AAFiCGJqRkttJVMVbFjj6KqTztfonbv9bro"	APP_ID = 1687098
+
+	API_HASH = "29e30652ac7722840afefa8ae015b513"
+
+	OWNER_ID = "832958166" #ID of bot owner
+
+	AUTH_CHANNEL = [-1001238677320]
+  
+  CUSTOM_FILE_NAME = "[TSNM]-"
+
+	DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
+
+	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+
+	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
